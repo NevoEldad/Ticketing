c14da8aaaf67b1da35ba15bd6a01b8cabe7582c3
NevoEldad <nevoeldad@gmail.com>
2021-01-17 17:00:05 +0200

    setup for easier tests

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
-import { signinRouter } from './routes/signin';
-import { errorHandler } from './middlswares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: false
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-
-app.all('*', async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
-
+import { app } from './app';
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('Could not get JWT_KEY');
